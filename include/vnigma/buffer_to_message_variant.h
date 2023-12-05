81548b5201abecbf6148dd59e07b869985d45bcb
decamel <den_spb97@mail.ru>
2023-12-05 12:39:43 +0300

    parsing function exported

--- include/vnigma/buffer_to_message_variant.h
+++ include/vnigma/buffer_to_message_variant.h
@@ -1,6 +1,7 @@
 #if !defined(VNIGMA_BUFFER_TO_MESSAGE_VARIANT)
 #define VNIGMA_BUFFER_TO_MESSAGE_VARIANT
 
+#include <vnigma/export.h>
 #include <vnigma/buffer.hpp>
 #include <vnigma/message/message_variant.hpp>
 
@@ -9,12 +10,12 @@ namespace vnigma {
 /**
  * @brief returns protocol from buffer
  */
-std::string_view get_protocol(buffer& buf);
+std::string_view VNIGMA_EXPORT get_protocol(buffer& buf);
 
 /**
  * @brief returns control string from buffer
  */
-std::string_view get_control(buffer& buf);
+std::string_view VNIGMA_EXPORT get_control(buffer& buf);
 
 /**
  * @brief converts buffer to message variant
@@ -22,7 +23,7 @@ std::string_view get_control(buffer& buf);
  * @param buf buffer to parse
  * @return message_variant one of messages
  */
-message_variant buffer_to_message_variant(buffer buf);
+message_variant VNIGMA_EXPORT buffer_to_message_variant(buffer buf);
 }  // namespace vnigma
 
 #endif  // VNIGMA_BUFFER_TO_MESSAGE_VARIANT
